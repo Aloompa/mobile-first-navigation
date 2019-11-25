8c6b3173227ec221780df47a1f5633e71958ddb8
Ben Miner <ben.miner@pop.belmont.edu>
2019-11-25 14:32:09 -0600

    Add React Native example WIP

---
+++ packages/mobile-first-navigation-example/native/Routes.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import Home from './routes/Home';
+// import Modal from './routes/Modal';
+// import Page2 from './routes/Page2';
+// import Page2b from './routes/Page2b';
+// import Page3 from './routes/Page3';
+import {always} from 'ramda';
+import {createRoutes} from '@aloompa/mobile-first-router/lib';
+import {TabButton, TopNav} from '@aloompa/mobile-first-components';
+
+export const routeConfig = {
+  initialActiveTab: 1,
+  initialRoute: 'Home',
+  tabs: [
+    {
+      button: (isSelected: boolean, onPress: Function) => (
+        <TabButton
+          selected={isSelected}
+          onPress={onPress}
+          iconUnselectedColor={'#ccc'}
+          iconHeight={18}
+          iconWidth={20}
+          iconSelectedColor={'#FF00FF'}
+          title={'Browse'}
+          iconUrl={
+            'https://aloompa-assets.imgix.net/TestFolder/reservations-tab-icon-browse.svg'
+          }
+        />
+      ),
+      initial: 'Home',
+    },
+    {
+      button: (isSelected: boolean, onPress: Function) => (
+        <TabButton
+          selected={isSelected}
+          onPress={onPress}
+          iconUnselectedColor={'#ccc'}
+          iconHeight={18}
+          iconWidth={22}
+          iconSelectedColor={'#FF00FF'}
+          title={'Home 2'}
+          iconUrl={
+            'https://aloompa-assets.imgix.net/TestFolder/reservations-tab-icon-tickets.svg'
+          }
+        />
+      ),
+      initial: 'Page2',
+    },
+    {
+      button: (isSelected: boolean, onPress: Function) => (
+        <TabButton
+          selected={isSelected}
+          onPress={onPress}
+          iconUnselectedColor={'#ccc'}
+          iconHeight={22}
+          iconWidth={22}
+          iconSelectedColor={'#FF00FF'}
+          title={'Home 3'}
+          iconUrl={
+            'https://aloompa-assets.imgix.net/TestFolder/reservations-tab-icon-account.svg'
+          }
+        />
+      ),
+      initial: 'Page3',
+    },
+  ],
+  routes: {
+    Home: {
+      route: Home,
+      getTitle: always(''),
+    },
+    // Page2: {
+    //   route: Page2,
+    //   getTitle: always('Page 2')
+    // },
+    // Page2b: {
+    //   route: Page2b,
+    //   getTitle: always('Page 2B')
+    // },
+    // Page3: {
+    //   route: Page3,
+    //   getTitle: always('Page 3 Home')
+    // },
+    // Modal: {
+    //   route: Modal,
+    //   getTitle: always('Modal'),
+    //   mode: 'modal'
+    // }
+  },
+  renderTopNav: TopNav,
+};
+
+const Routes = createRoutes(routeConfig);
+
+export default Routes;
---
+++ packages/mobile-first-navigation-example/native/routes/Home.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import {Text, Button, View} from '@aloompa/mobile-first-components';
+
+const Home = (props: any) => (
+  <View
+    style={{
+      display: 'flex',
+      flex: 1,
+      height: '100%',
+      flexDirection: 'column',
+    }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#444444',
+        padding: 12,
+      }}>
+      <Text
+        style={{
+          color: '#FFFFFF',
+        }}>
+        Home
+      </Text>
+    </View>
+    <View
+      style={{
+        height: 50,
+        width: '100%',
+        backgroundColor: '#CCC',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Button
+        style={{
+          height: 50,
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onClick={e => props.nextPage(e)}>
+        <View>
+          <Text>Page 2</Text>
+        </View>
+      </Button>
+    </View>
+  </View>
+);
+
+export default (props: any) =>
+  Home({
+    ...props,
+    nextPage: e => {
+      e.preventDefault();
+      props.setRoute({
+        route: 'Page2',
+        params: {},
+      });
+    },
+  });
